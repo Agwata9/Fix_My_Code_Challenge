b77839dc43bfb43baa746418f4c6c7f284c5c5f0
agwata9 <agwata9@gmail.com>
2023-02-04 00:03:15 -0800

    submision

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+# Calculate the square of given values
 
-class Square():
-    
+
+class Square:
+    # Aclass doing the magic
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
+    def permiter_Of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_Of_my_square())
